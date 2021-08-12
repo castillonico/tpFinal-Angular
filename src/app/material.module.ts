2d5747de495cefacfa53404494061cdd9afd953f
castillonico <nico.a.castillo@gmail.com>
2021-08-12 04:09:44 -0300

    Agrego campos al formulario de registro

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -2,13 +2,26 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common'; 
 
 import { MatButtonModule } from '@angular/material/button'; 
-
-
+import {MatFormField, MatFormFieldModule} from '@angular/material/form-field'; 
+import {MatInputModule} from '@angular/material/input'; 
+import {ErrorStateMatcher} from '@angular/material/core';
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule, 
+    MatButtonModule, 
+    MatFormFieldModule, 
+    MatFormField, 
+    MatInputModule, 
+    ErrorStateMatcher
+  ], 
+  exports: [ 
+    MatButtonModule, 
+    MatFormFieldModule, 
+    MatFormField, 
+    MatInputModule, 
+    ErrorStateMatcher
   ]
 })
 export class MaterialModule { }
--- src/app/public/register/register.component.ts
+++ src/app/public/register/register.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'; 
+import { FormControl, FormGroup } from '@angular/forms';
+import { RegisterService } from 'src/app/services/register.service';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +9,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  userAuth: any; 
+  regForm = new FormGroup ({ 
+    newName: new FormControl(), 
+    newLastname: new FormControl(),  
+    newPassword: new FormControl(), 
+    newPhone: new FormControl(),
+    newEmail: new FormControl()
+  })
+  constructor( private service: RegisterService ) { }
 
   ngOnInit(): void {
   }
+
+  createUser () {
+    this.service.registerUser( 
+      this.regForm.value.newName, 
+      this.regForm.value.newLastname, 
+      this.regForm.value.newPassword, 
+      this.regForm.value.newPhone, 
+      this.regForm.value.newEmail 
+      createUser () {
+        this.service.registerUser( 
+          this.regForm.value.newName, 
+          this.regForm.value.newLastname, 
+          this.regForm.value.newPassword, 
+          this.regForm.value.newPhone, 
+          this.regForm.value.newEmail
+        ).subscribe ( (response:any) => {
+          this.userAuth = response; 
+          console.log("El usuario se está creando... "); 
+          console.log("El usuario es: ", this.userAuth);
+        }); 
+      };  
+    ).subscribe ( (response:any) => {
+      this.userAuth = response; 
+      console.log("El usuario se está creando... "); 
+      console.log("El usuario es: ", this.userAuth);
+    }); 
+  }; 
 
 }
